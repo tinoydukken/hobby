28aec4a117b98e725a7cdce7a1d582fa70a71241
tinoydukken <tinoydukken@gmail.com>
2021-01-07 20:47:02 +0530

    intial commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#createbtn").hover(function(){
+      $("#createtxt").toggle(1500);
+      
+    });
+  });
+  $(document).ready(function(){
+    $("#modernbtn").hover(function(){
+      $("#moderntxt").toggle(1500);
+      
+    });
+  });
+  $(document).ready(function(){
+    $("#digitalbtn").hover(function(){
+      $("#digitaltxt").toggle(1500);
+      
+    });
+  });
